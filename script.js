93e0d0ae93d148ddaf7357a6b96d73bf3ef0491c
wojakcupi2007 <120767273+wojakcupi2007@users.noreply.github.com>
2023-12-16 01:31:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const API_KEY = "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";
+const prompt = document.querySelector("#busqueda");
+const button = document.querySelector("#generate");
+const res = document.querySelector("#res");
+
+async function getCompletion(prompt) {
+  const response = await fetch(`https://api.openai.com/v1/completions`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: "informacion de la cancion o compositor de"+prompt,
+      max_tokens: 150,
+
+    }),
+  });
+function animateImage() {
+  img.style.animation = "uwu infinite 20s linear";
+}
+  const data = await response.json();
+  return data;
+}
+
+button.addEventListener("click", async () => {
+  const response = await getCompletion(prompt.value);
+  res.innerHTML = response.choices[0].text;
+});
